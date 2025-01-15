4c13b843363e919db527904006b6b51fd6c33b99
Manal Mukarram <manalmukarram18@gmail.com>
2025-01-15 19:03:17 +0530

    fixed code with const

--- lib/material_page.dart
+++ lib/material_page.dart
@@ -4,7 +4,7 @@ class MyMaterialPage extends StatelessWidget {
   const MyMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(child: Text('Hello World..')),
     );
   }
