bee420bf318bd43686b6ed8ccadf7d9124c267c0
Manal Mukarram <manalmukarram18@gmail.com>
2025-01-17 00:56:57 +0530

    created my customizable border - myBorder

--- lib/material_page.dart
+++ lib/material_page.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 class MyMaterialPage extends StatelessWidget {
   const MyMaterialPage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    const myBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(50),
+      ),
+      borderSide: BorderSide(color: Colors.white, width: 1),
+    );
     return const Scaffold(
         backgroundColor: Colors.black,
         body: Center(
@@ -17,23 +24,20 @@ class MyMaterialPage extends StatelessWidget {
               TextField(
                 style: TextStyle(color: Colors.white),
                 decoration: InputDecoration(
-                  hintText: 'Please enter the amount in INR here.',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  filled: true,
-                  fillColor: Colors.white30,
-                  prefixIcon: Icon(
-                    Icons.currency_rupee,
-                    color: Color.fromARGB(255, 162, 195, 223),
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50))),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(50),
+                    hintText: 'Please enter the amount in INR here.',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: Colors.white30,
+                    prefixIcon: Icon(
+                      Icons.currency_rupee,
+                      color: Color.fromARGB(255, 162, 195, 223),
                     ),
-                    borderSide: BorderSide(color: Colors.white, width: 1),
-                  ),
-                ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                    ),
+                    focusedBorder: myBorder),
               ),
             ],
           ),
