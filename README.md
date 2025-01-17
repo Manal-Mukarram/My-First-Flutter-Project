cfe12213799337e0478b6e1f641961f2ba4766a6
Manal Mukarram <manalmukarram18@gmail.com>
2025-01-17 05:52:25 +0530

    tried getting a result value using stateless widget, which didn't work, hence understood the need of stateful widget

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # currency_converter
 
-A new Flutter project.
+A new Flutter project. This is my first flutter project, and I really hope I work great on this. I'm trying my best and I really wish my hardwork would get me what I need. I'm pretty sure no one would read my frist project's readme file except me. So I'm letting it all out here. I hope I would make him happy with my work. My life is unpredictable right now, and I have a strong desire for my wish to be true. I don't think I've given it my best yet. I need to really work on what I wish for, I need to "achieve" it, afterall, the meaning of my name is "achievement" or "accomplishment". After what has happened in my life until now, I've realised many things in life. Such as, never prioritize anything or anyone over deen. Love myself no matter what, I'm worthy enough and I should not at all care about what others think about me, this was taught to me by someone, someone so special, who helped me realise that I'm not worthless, my opinions matter, my feelings matter, I...matter. Yes, I'm crying and I'm sad. But I hope future me wouldn't be. I hope I don't have any regrets in future, where I feel I never pushed myself hard enough. Good Luck :sed
+
+Getting back to work, just wondering if I need to use // to comment or something, or I'm free to write anything here 
+Okay so the steps for the app are:
+1. Creating a variable that stores the converted currency value
+2. Create a function that multiplies the value given by textfield with 81 (USD to INR) 
+3. Store the value in the variable created
+4. Display the value (converted amount)
+
 
 ## Getting Started
 
--- lib/material_page.dart
+++ lib/material_page.dart
@@ -5,6 +5,8 @@ class MyMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double convertedValue = 0;
+    final TextEditingController textEditingController = TextEditingController();
     const myBorder = OutlineInputBorder(
       borderRadius: BorderRadius.all(
         Radius.circular(50),
@@ -12,27 +14,36 @@ class MyMaterialPage extends StatelessWidget {
       borderSide: BorderSide(color: Colors.white, width: 1),
     );
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: const Color.fromARGB(255, 150, 149, 149),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 116, 115, 115),
+        centerTitle: true,
+        title: const Text('Currency Converter'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Hello World',
-              style: TextStyle(color: Colors.white),
+            // CONVERTED VALUE
+            Text(
+              convertedValue.toString(),
+              style: const TextStyle(color: Colors.white, fontSize: 40),
             ),
+
+            // INR AMOUNT TEXTFIELD
             Container(
-              padding: const EdgeInsets.fromLTRB(30, 15, 30, 15),
-              child: const TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
+              padding: const EdgeInsets.fromLTRB(100, 15, 100, 15),
+              child: TextField(
+                controller: textEditingController,
+                style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                decoration: const InputDecoration(
                   hintText: 'Please enter the amount in INR here.',
-                  hintStyle: TextStyle(color: Colors.grey),
+                  hintStyle: TextStyle(color: Color.fromARGB(255, 95, 95, 95)),
                   filled: true,
                   fillColor: Colors.white30,
                   prefixIcon: Icon(
                     Icons.currency_rupee,
-                    color: Color.fromARGB(255, 162, 195, 223),
+                    color: Color.fromARGB(255, 0, 0, 0),
                   ),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.all(
@@ -41,11 +52,34 @@ class MyMaterialPage extends StatelessWidget {
                   ),
                   focusedBorder: myBorder,
                 ),
-                keyboardType: TextInputType.numberWithOptions(
+                keyboardType: const TextInputType.numberWithOptions(
                   decimal: true,
                 ),
               ),
             ),
+
+            // CONVERTER BUTTON
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 15, 30,
+                  15), // added padding in case I need to use button in rectangular shape
+              child: ElevatedButton(
+                onPressed: () => convertedValue =
+                    double.parse(textEditingController.text) * 81,
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.white),
+                  foregroundColor: MaterialStatePropertyAll(
+                    Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  shape: MaterialStatePropertyAll(CircleBorder()),
+                  elevation: MaterialStatePropertyAll(5),
+                  shadowColor: MaterialStatePropertyAll(
+                    Color.fromARGB(255, 0, 0, 0),
+                  ), // in case bg is black, use white shadow
+                  iconSize: MaterialStatePropertyAll(50),
+                ),
+                child: const Icon(Icons.change_circle),
+              ),
+            )
           ],
         ),
       ),
